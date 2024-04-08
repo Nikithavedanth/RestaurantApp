4397388746e1d629fb10300b702a90e7d0d4ce96
Nikithavedanth <nikithavedanth@gmail.com>
2024-04-08 14:54:59 -0400

    Updated

--- projects/rest-web/src/app/app.component.ts
+++ projects/rest-web/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { TabComponent } from './tab/tab.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TabComponent,HomeComponent],
+  imports: [RouterOutlet, TabComponent,HomeComponent,MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
